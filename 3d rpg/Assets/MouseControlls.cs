754bc92139071580223459e00f0bf729768e021b
Paul B <p_botusan@live.nl>
2020-03-26 20:25:29 +0100

    particles, mousepointer, follow camera

---
+++ 3d rpg/Assets/MouseControlls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControlls : MonoBehaviour
+{
+    /// <summary>
+    /// Texture used for mousecursor
+    /// </summary>
+    public Texture2D cursorTexture;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private CursorMode mode = CursorMode.ForceSoftware;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private Vector2 hotSpot = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, mode);
+        
+    }
+}
---
+++ 3d rpg/Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    /// <summary>
+    /// Camera follow height
+    /// </summary>
+    [SerializeField]
+    private float followHeight = 8f;
+
+    /// <summary>
+    /// Distance for follow camera
+    /// </summary>
+    [SerializeField]
+    private float followDistance = 6f;
+
+    /// <summary>
+    /// Use player transform for current player position
+    /// </summary>
+    [SerializeField]
+    private Transform player;
+
+    /// <summary>
+    /// target height
+    /// </summary>
+    private float targetHeight;
+
+    /// <summary>
+    /// Current height camera
+    /// </summary>
+    private float currentHeight;
+
+    /// <summary>
+    /// Current rotation camera
+    /// </summary>
+    private float currentRotation;
+
+  
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        MoveCamera();
+    }
+
+    /// <summary>
+    /// translate target height from playerposition add followheigt for correct position
+    /// change the current rotation and transform
+    /// change the currentheight transform to target height, mathf.lerp smooth zoom ins and outs
+    /// change the rotation for camera
+    /// change the target position to the player position, follow player.
+    /// translate the camera to follow the player
+    /// </summary>
+    private void MoveCamera()
+    {
+        targetHeight = player.position.y + followHeight;
+        currentRotation = transform.eulerAngles.y;
+        currentHeight = Mathf.Lerp(transform.position.y, targetHeight, 0.9f * Time.deltaTime);
+        Quaternion euler = Quaternion.Euler(0f, currentRotation, 0f);
+
+        Vector3 targetPosition = player.position - (euler * Vector3.forward) * followDistance;
+        targetPosition.y = currentHeight;
+        transform.position = targetPosition;
+        transform.LookAt(player);
+    }
+}
