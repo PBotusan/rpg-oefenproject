89aa881dbee3017c6639c96f6499987e390c8fa6
Paul B <p_botusan@live.nl>
2020-08-27 01:55:51 +0200

    mouse pointer + mousecursor

---
+++ 3d rpg/Assets/DestroyPointer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPointer : MonoBehaviour
+{
+    private Transform player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DestroyMousePointer();
+    }
+
+    /// <summary>
+    /// Destroy mouse pointer when player position is more than 1.1f
+    /// </summary>
+    private void DestroyMousePointer()
+    {
+        Destroy(gameObject, 1f);
+    }
+}
--- 3d rpg/Assets/MouseControlls.cs
+++ 3d rpg/Assets/MouseControlls.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +8,40 @@ public class MouseControlls : MonoBehaviour
     /// <summary>
     /// Texture used for mousecursor
     /// </summary>
-    public Texture2D cursorTexture;
+    [SerializeField] Texture2D cursorTexture;
+
+
+    [SerializeField] GameObject mousePoint;
 
     /// <summary>
-    /// 
+    /// mode van cursor soort
     /// </summary>
     private CursorMode mode = CursorMode.ForceSoftware;
 
     /// <summary>
-    /// 
+    /// plek waar je op drukt ingame.
     /// </summary>
     private Vector2 hotSpot = Vector2.zero;
+
+    /// <summary>
+    /// instantiate mouse position
+    /// </summary>
+    private GameObject instantiateMouse;
+
+    /// <summary>
+    /// when player clicks left mouse button
+    /// </summary>
+    private int mouseButtonDown = 1;
+
+    /// <summary>
+    /// when player releases mouse click.
+    /// </summary>
+    private int mouseButtonUp = 0;
+
+    /// <summary>
+    /// the height of the terrain in y axis .
+    /// </summary>
+    private float TerrainHeight = 15.4f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +53,28 @@ public class MouseControlls : MonoBehaviour
     void Update()
     {
         Cursor.SetCursor(cursorTexture, hotSpot, mode);
-        
+
+
+        if (Input.GetMouseButtonUp(mouseButtonUp))
+        {
+            PlaceMouseCursor();
+        }
+    }
+
+    private void PlaceMouseCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider is TerrainCollider)
+            {
+                Vector3 temp = hit.point;
+                temp.y = TerrainHeight;
+
+                Instantiate(mousePoint, temp, Quaternion.identity);
+            }
+        }
     }
 }
